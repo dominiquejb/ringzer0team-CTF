44d4450408686f0e99f25da0f7bc6b015696fdb7
dBizzle <dominicbri7@gmail.com>
2015-05-26 10:27:20 -0400

    Challenge 57 code + write-up (hash breaker reloaded)

--- challenge57/hashbreakerreloaded.py
+++ challenge57/hashbreakerreloaded.py
@@ -7,26 +7,36 @@ __author__ = 'dBizzle'
 s = requests.session()
 
 #url to fetch
-baseurl = "http://ringzer0team.com/challenges/56"
+baseurl = "http://ringzer0team.com/challenges/57"
 
 #fetch the web page with my PHPSESSID at the time
 r = s.post(baseurl, cookies={'PHPSESSID': 'j3c7p18vekvafqev0cnejq4g53'})
 
-#find area around the message i need, reduces string size for regex to search in
-ibegin = r.content.find("BEGIN HASH -----") + 16 #index+length
-iend = r.content.find("----- END HASH") #index
-msgsection = r.content[ibegin:iend]
+#find area around the hash, reduces string size for regex to search in
+hashbegin = r.content.find("BEGIN HASH -----") + 16 #index+length
+hashend = r.content.find("----- END HASH") #index
+hashsection = r.content[hashbegin:hashend]
 
-#Extract just the message from the page
-msg = re.findall(r"\s+(\w+)<br", msgsection)[0]
+#find area around the salt, reduces string size for regex to search in
+saltbegin = r.content.find("BEGIN SALT -----") + 16 #index+length
+saltend = r.content.find("----- END SALT") #index
+saltsection = r.content[saltbegin:saltend]
+
+#Extract just the hash from the page
+theHash = re.findall(r"\s+(\w+)<br", hashsection)[0]
+print theHash
+
+#Extract just the salt from the page
+salt = re.findall(r"\s+(\w+)<br", saltsection)[0]
+print salt
 
 #After using online reverse sha1 tools,
 # we found that the decrypted hash is an int value with 4 digits (1000-9999)
 # let's brute-force it since it's easy to find
 for i in xrange(1000,10000):
-    if (hashlib.sha1(str(i)).hexdigest() == msg):
+    if (hashlib.sha1(str(i)+str(salt)).hexdigest() == theHash):
         answer = str(i)
-        print "answer = " + answer
+        print "1answer = " + answer
         break
 
 
